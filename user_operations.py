4e8ed680d821a524b03432965f77c4cd94c5c357
joshuaharris07 <joshua.harris07@gmail.com>
2024-10-29 11:57:45 -0600

    Finished user_operations and updating the methods.

--- user_operations.py
+++ user_operations.py
@@ -1,30 +1,68 @@
+from connect_mysql import connect_database
 import random
 
 class UserOperations:
-    def __init__(self):
-        self.users = {
-            "1234567": {"library id": "1234567", "name": "John Doe", "borrowed books": []},
-            "7777777": {"library id": "7777777", "name": "Moby Dog", "borrowed books": ["Mistborn", "The Codex Alera"]}
-        }
-    
     def add_user(self, name):
         while True:  
             library_id_generator = [str(random.randint(0, 9)) for _ in range(7)]   # If additional numbers are needed, increase the range.
             library_id = "".join(library_id_generator)
-            if library_id not in self.users: 
-                break
-        self.users[library_id] = {"library id": library_id, "name": name, "borrowed books": []}
-        print(f"New user: {name} has been assigned the following library ID: {library_id}")
 
-    def borrow_book(self, library_id, title):
-        self.users[library_id]["borrowed books"].append(title)
+            conn = connect_database()
+            if conn is not None:
+                try:
+                    cursor = conn.cursor()
+                    cursor.execute(f"SELECT id from Users WHERE library_id = '{library_id}'")
+                    if cursor.fetchone() == None:
+                        print(f"{library_id} is not yet in use.")
+                        query = "INSERT INTO Users (name, library_id) VALUE (%s, %s)"
+                        add_user = (name, library_id)
+                        cursor.execute(query, add_user)
+                        conn.commit()
+                        print(f"Welcome, {name}. Your account has been created. Your Library ID is: {library_id}\nReturning to the main menu.")
+                        break
+
+                except Exception as e:
+                    print(f"Error: {e}")
+
+                finally:
+                    cursor.close()
+                    conn.close()
+
 
     def view_user(self, library_id): 
-        if library_id in self.users:
-            print(f"Name: {self.users[library_id]["name"]}. Currently borrowing: {", ".join(self.users[library_id]["borrowed books"])}")
-        else:
-            print("That library ID was not found in the system. Returning to the menu.")
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"SELECT name from Users WHERE library_id = '{library_id}'")                
+                if cursor.fetchone() == None:
+                    print("That library ID was not found in the system. Returning to the menu.")
+                else:
+                    cursor.execute(f"SELECT name from Users WHERE library_id = '{library_id}'")  
+                    for user in cursor.fetchall():
+                        (name, ) = user
+                        print(f"{library_id} is assigned to {name}.")
+                    
+            except Exception as e:
+                print(f"Error: {e}")
+
+            finally:
+                cursor.close()
+                conn.close()
 
     def view_all_users(self):
-        for details in self.users.values():
-            print(f"Library ID: {details["library id"]}, Name: {details["name"]}, Borrowed Books: {", ".join(details["borrowed books"])}")
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM Users")
+                for user in cursor.fetchall():
+                    (user_id, user_name, library_id) = user
+                    print(f"User: {user_name}. Library ID: {library_id}")
+                    
+            except Exception as e:
+                print(f"Error: {e}")
+
+            finally:
+                cursor.close()
+                conn.close()
