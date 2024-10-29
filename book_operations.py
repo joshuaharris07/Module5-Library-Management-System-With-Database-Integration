8030e2becdf68dd477b44e38d6a1f40df6dfb445
joshuaharris07 <joshua.harris07@gmail.com>
2024-10-29 10:31:51 -0600

    Added formatting changes to main.py to improve visibility of main menu. Finished putting together the book_operations class.

--- book_operations.py
+++ book_operations.py
@@ -1,77 +1,179 @@
-class BookOperations:
-    def __init__(self):
-        self.books = {
-            "Lord Of The Rings": {"title": "Lord Of The Rings", "author": "Tolkien", "genre": "Fantasy", "publication year": 1954, "availability": True},
-            "Mistborn": {"title": "Mistborn", "author": "Sanderson", "genre": "Fantasy", "publication year": 2006, "availability": False},
-            "The Codex Alera": {"title": "The Codex Alera", "author": "Butcher", "genre": "Fantasy", "publication year": 2004, "availability": False},
-            "1984": {"title": "1984", "author": "Orwell", "genre": "Fiction", "publication year": 2004, "availability": True}
-            }
-    
+from connect_mysql import connect_database
+from datetime import date, timedelta
+
+class BookOperations:    
     def add_book(self):
         while True:
             title = input("Enter the title of the book: ").title()
             if title.strip() == "":
                 print("No book title was entered. Returning to the main menu.")
                 break
-            if title in self.books:
-                print("That book is already in the library. Returning to the menu.")
             author = input("Enter the author's last name: ").title()
             if author.strip() == "":
                 print("No author was entered. Returning to the main menu.")
                 break
-            genre = input("Enter the genre of the book: ").title()
-            if genre.strip() == "":
-                print("No genre was entered. Returning to the main menu.")
+
+            conn = connect_database()   # Confirming author is in the database.
+            if conn is not None:
+                try:
+                    cursor = conn.cursor()
+                    cursor.execute(f"SELECT id from Authors WHERE name = '{author}'")
+                    for author in cursor.fetchall():
+                        (author_id, ) = author  # Pulls the id from Authors in SQL
+                except Exception as e:
+                    print(f"Error: {e}")
+
+                finally:
+                    cursor.close()
+                    conn.close()
+                    
+            # TODO Should it auto add an author if not found in the system? Auto-fill biography to "Wrote {book}."?
+            isbn = input("Enter the 13 digit ISBN of the book: ") 
+            if isbn.strip() == "":
+                print("No ISBN was entered. Returning to the main menu.")
                 break
-            try: 
-                publication_year = int(input("Enter the year the book was published: "))  
-            except: 
-                print("Invalid publication year. Returning to the menu.")
-                break
-            self.books[title] = {"title": title, "author": author, "genre": genre, "publication year": publication_year, "availability": True}
+            publication_date = input("Enter the date the book was published in this format: YYYY-MM-DD: ")  # TODO verification for way the year was entered. Maybe change to just year?
+            
+            conn = connect_database() #TODO can I get rid of this? Maybe add it to the main class? Maybe to the beginning of the main file, it won't run anything unless it can connect?
+            if conn is not None:
+                try:
+                    cursor = conn.cursor()
+                    query = f"""
+                    INSERT INTO Books (title, author_id, isbn, publication_date)
+                    VALUES ({title}, {author_id}, {isbn}, {publication_date});
+                    """
+                    cursor.execute(query)
+
+                except Exception as e:
+                    print(f"Error: {e}")
+
+                finally:
+                    cursor.close()
+                    conn.close()           
             print(f"{title} was successfully added to the system.")
             break
 
-    def borrow_book(self, title, library_id, user_operations):
-        if title in self.books and self.books[title]["availability"]:
-            if library_id in user_operations.users: 
-                user_operations.borrow_book(library_id, title)
-                print(f"You have checked {title} out. Returning to the menu")
-                self.books[title]["availability"] = False
-            else:
-                print("Library ID is invalid.")
-        elif title in self.books:
-            print(f"{title} is already checked out and is not currently available.")
-        else:
-            print(f"{title} is not currently in our library system. Returning to the menu.")
+    def borrow_book(self, title, library_id):
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"SELECT id, availability from Books WHERE title = '{title}'")
+                for book in cursor.fetchall():
+                    (book_id, availability) = book
+                find_library_id = (library_id, )
+                query = "SELECT id, name from Users WHERE library_id = %s"
+                cursor.execute(query, find_library_id)
+                for user in cursor.fetchall():
+                    (user_id, user_name) = user
 
-    def return_book(self, title, user_operations):
-        if title in self.books and not self.books[title]["availability"]:
-            self.books[title]["availability"] = True
-            for details in user_operations.users.values():
-                if title in details["borrowed books"]:
-                    details["borrowed books"].remove(title)
-                    print(f"{title} has been successfully returned. Returning to the menu.")
-        elif title in self.books:
-            print(f"That book wasn't checked out yet! Returning to the menu.")
-        else:
-            print(f"{title} was not found in the library's system. Returning to the menu.")
+                if availability == 1:
+                    cursor.execute(f"""
+                    UPDATE books 
+                    SET availability = '0'
+                    WHERE id = {book_id}
+                    """)
+                    conn.commit()
+            
+                    query = ("""
+                    INSERT INTO Borrowed_books (user_id, book_id, borrow_date, return_date) VALUES (%s, %s, %s, %s)
+                    """)
+                    return_date = date.today() + timedelta(weeks=3)
+                    borrow_book = (user_id, book_id, date.today(), return_date)
+                    cursor.execute(query, borrow_book)
+                    conn.commit()
+                    print(f"{user_name} has checked out {title}. The due date is {return_date}")
+
+                else:
+                    print(f"{title} is not currently available.") #TODO list when it should be available
+
+            except Exception as e:
+                print(f"Error: {e}")
+
+            finally:
+                cursor.close()
+                conn.close()
+
+    def return_book(self, title):
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"SELECT id, availability from Books WHERE title = '{title}'")
+                for book in cursor.fetchall():
+                    (book_id, availability) = book  # Pulls the id from Authors in SQL
+
+                if availability != 1:
+                    cursor.execute(f"""
+                    UPDATE Books 
+                    SET availability = '1'
+                    WHERE id = {book_id}
+                    """)
+                    conn.commit()
+                    
+                    cursor.execute(f"""
+                    DELETE FROM Borrowed_books
+                    WHERE book_id = {book_id}               
+                    """)
+                    conn.commit()
+
+                    print(f"{title} has been returned.")
+                
+                else:
+                    print(f"{title} was not previously checked out.")
+
+            except Exception as e:
+                print(f"Error: {e}")
+
+            finally:
+                cursor.close()
+                conn.close()
 
     def search_book(self, title):
-        if title in self.books:
-            print("Currently available for checkout.") if self.books[title]["availability"] else print("This book is currently checked out.")
-            print(f"{title}, Author {self.books[title]["author"]}, Genre {self.books[title]["genre"]}, Publication Year: {self.books[title]["publication year"]}")
-        else:
-            print(f"{title} is not currently in our library.")
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f"""
+                SELECT b.title AS Title, a.name AS Author, b.isbn AS ISBN, b.publication_date, b.availability
+                FROM Books b, Authors a
+                WHERE b.author_id = a.id AND b.title LIKE '%{title}%';
+                """)
+                for book in cursor.fetchall():
+                    (title, author_name, ISBN, publication_date, availability) = book
+                    if availability == 1:
+                        print(f"\nTitle: {title}\nAuthor: {author_name}\nISBN: {ISBN}\nPublication Date: {publication_date}\n{title} is currently available for checkout.")
+                    else:
+                        print(f"\nTitle: {title}\nAuthor: {author_name}\nISBN: {ISBN}\nPublication Date: {publication_date}\n{title} is not currently available for checkout.")
+
+            except Exception as e:
+                print(f"Error: {e}")
+
+            finally:
+                cursor.close()
+                conn.close()
+                print("Connection closed.")
 
     def display_all_books(self):
-        available_books = []
-        not_available_books = []
-        for book in self.books.values():
-            available_books.append(book) if book["availability"] else not_available_books.append(book)
-        print("\nCurrently available books:")
-        for book in available_books:
-            print(f"{book["title"]}, Author: {book["author"]}, Genre: {book["genre"]}, Publication Year: {book["publication year"]}")
-        print("\nBooks that are currently checked out:")
-        for book in not_available_books:
-            print(f"{book["title"]}, Author: {book["author"]}, Genre: {book["genre"]}, Publication Year: {book["publication year"]}")
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                cursor.execute("""
+                SELECT b.title AS Title, a.name AS Author, b.isbn AS ISBN, b.publication_date, b.availability
+                FROM Books b, Authors a
+                WHERE b.author_id = a.id;
+                """)
+                for book in cursor.fetchall():
+                    (title, author_name, ISBN, publication_date, availability) = book
+                    if availability == 1:
+                        print(f"\nTitle: {title}, Author: {author_name}, ISBN: {ISBN}, Publication Date: {publication_date}. Currently available.")
+                    else:
+                        print(f"\nTitle: {title}, Author: {author_name}, ISBN: {ISBN}, Publication Date: {publication_date}. Not available.")
+
+            except Exception as e:
+                print(f"Error: {e}")
+
+            finally:
+                cursor.close()
+                conn.close()
--- main.py
+++ main.py
@@ -14,23 +14,23 @@ while True:
 3. Author Operations
 4. Quit\n""")
     if menu_action == "1":
-        menu_action = input("Book Operations:\n1. Add a new book\n2. Borrow a book\n3. Return a book\n4. Search for a book\n5. Display all books\n")
+        menu_action = input("\nBook Operations:\n1. Add a new book\n2. Borrow a book\n3. Return a book\n4. Search for a book\n5. Display all books\n")
         if menu_action == "1": 
             book_operations.add_book()
         elif menu_action == "2":
             title = input("Enter the title of the book you wish to check out: ").title()
             library_id = input("Please enter your library ID: ").strip()
-            book_operations.borrow_book(title, library_id, user_operations)
+            book_operations.borrow_book(title, library_id)
         elif menu_action == "3":
             title = input("Enter the title of the book you wish to return: ").title()
-            book_operations.return_book(title, user_operations)
+            book_operations.return_book(title)
         elif menu_action == "4":
-            title = input("Enter the title of the book you wish to look for: ").title()
+            title = input("Enter the title of the book (or part of the title) you wish to look for: ").title()
             book_operations.search_book(title)
         elif menu_action == "5":
             book_operations.display_all_books()
     elif menu_action == "2":
-        menu_action = input("User Operations:\n1. Add a new user\n2. View user details\n3. Display all users\n")
+        menu_action = input("\nUser Operations:\n1. Add a new user\n2. View user details\n3. Display all users\n")
         if menu_action == "1": 
             name = input("Please put in your full name: ").title()
             if name.strip() == "":
@@ -43,7 +43,7 @@ while True:
         elif menu_action == "3":
             user_operations.view_all_users()
     elif menu_action == "3":
-        menu_action = input("Author Operations:\n1. Add a new author\n2. View author detais\n3. Display all authors\n")
+        menu_action = input("\nAuthor Operations:\n1. Add a new author\n2. View author detais\n3. Display all authors\n")
         if menu_action == "1": 
             author_operations.add_author()
         elif menu_action == "2":
@@ -52,7 +52,7 @@ while True:
         elif menu_action == "3":
             author_operations.view_all_authors()
     elif menu_action == "4":
-        print("Thank you for using the library management system. Have a good day!")
+        print("\nThank you for using the library management system. Have a good day!")
         break
     else:
-        print("Please make a selection by entering an integer.")
+        print("\nPlease make a selection by entering an integer.")
